d6eee617e3d3588127e102d2d7e09553d665d38c
tomoss <ionut.tomoss@gmail.com>
2018-03-31 20:18:57 +0300

    First commit

--- AutoX/Controllers/HomeController.cs
+++ AutoX/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace AutoX.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact PAGEE.";
 
             return View();
         }
